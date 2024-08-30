79421ed2a9dabee82abc529d14bcc0e1c9c4fa5c
Victor Barbosa <victoraug09@gmail.com>
2024-08-30 10:46:45 -0300

    Update main.py
    small fix

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from termcolor import colored
 
 openai.api_key = "API-key-openai"
 
-display_LolTracker()
-
 def prompt(pergunta): # função que faz o prompt para perguntar ao GPT
     model_engine = "text-davinci-002"
     # modelo GPT-3 que será utilizado, davinci é o mais confiável    
@@ -24,26 +22,28 @@ def prompt(pergunta): # função que faz o prompt para perguntar ao GPT
 # Tela inicial com as opções
 # Função para exibir o texto RedMoon em ASCII art
 def display_LolTracker():
-    Lol_ascii_art = pyfiglet.figlet_format("Lol", font="epic")
-    Tracker_ascii_art = pyfiglet.figlet_format("Tracker", font="epic")
+    Lol_ascii_art = pyfiglet.figlet_format("Lol", font="bell")
+    Tracker_ascii_art = pyfiglet.figlet_format("Tracker", font="bell")
     red_colored = colored(Lol_ascii_art, 'red')
     combined_art = ""
-    for Lol_line, Tracker_line in zip(Lol_colored.split('\n'), Tracker_ascii_art.split('\n')):
+    for Lol_line, Tracker_line in zip(red_colored.split('\n'), Tracker_ascii_art.split('\n')):
         combined_art += Lol_line + Tracker_line + "\n"
     print(combined_art)
 
-tipopartida = input("Bem vindo ao LolTracker!\nOnde ninguém aposta no escuro...\n Escolha uma opção:\n1. Melhor de 3\n 2. Melhor de 5\n3.  Partida unica ")
+display_LolTracker()
+tipopartida = input("Bem vindo ao LolTracker!\nOnde ninguém aposta no escuro...\n Escolha uma opção:\n1. Melhor de 3\n2. Melhor de 5\n3. Partida unica\n ")
                                                     
-if (tipopartida = 1){
+if (tipopartida == 1):
   md = ("melhor de 3")
-}
-elif (tipopartida = 2){
-  md = ("melhor de 5")
-}
-elif (tipopartida = 3){
-  md = ("partida unica")
-}
 
+elif (tipopartida == 2):
+  md = ("melhor de 5")
+
+elif (tipopartida == 3):
+  md = ("partida unica")
+
+md = tipopartida
+pergunta = "a"
 timex = input("Digite o primeiro time: ")
 timey = input("Digite o segundo time: ")
 resposta = prompt(pergunta)
